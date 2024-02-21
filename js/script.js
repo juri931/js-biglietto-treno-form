7238ea19023af25d831b8105c929c8bed632aae4
juri931 <impa.mat@hotmail.it>
2024-02-21 17:40:53 +0100

    [fix] some js code

--- js/script.js
+++ js/script.js
@@ -5,33 +5,34 @@ document.getElementById('myForm').addEventListener('submit', function (event) {
   const distanceKm = document.getElementById('inputKm');
   const ageSelect = document.getElementById('formSelect');
 
-  console.log(`Name: ${userName.value}`);
-  console.log(`Distance: ${distanceKm.value} KM`);
-  console.log(`Age: ${ageSelect.value}`);
+  console.log(`Nome: ${userName.value}`);
+  console.log(`Distanza: ${distanceKm.value} KM`);
+  console.log(`Età: ${ageSelect.value}`);
 
-  document.getElementById('userName').textContent = userName.value;
-  document.getElementById('distance').textContent = distanceKm.value;
-  document.getElementById('age').textContent = ageSelect.value;
+  userName.textContent = userName.value;
+  distanceKm.textContent = distanceKm.value;
+  ageSelect.textContent = ageSelect.value;
 
   const basePricePerKm = 0.21;
   const totalDistance = parseFloat(distanceKm.value);
   let total = totalDistance * basePricePerKm;
 
-  document.getElementById('total').textContent = `The cost of the ride will be: €${total.toFixed(2)}`;
+  document.getElementById('total').textContent = `Costo della tratta: €${total.toFixed(2)}`;
 
   let discount = 0;
 
-  if (ageSelect.value < 18) {
+
+  if (ageSelect.value = minor) {
     discount = total * 0.20;
-    document.getElementById('discount').textContent = `Your 20% discount is: €${discount.toFixed(2)}`;
-  } else if (ageSelect.value > 65) {
+    document.getElementById('discount').textContent = `Il tuo sconto del 20% è di: €${discount.toFixed(2)}`;
+  } else if (ageSelect.value = major) {
     discount = total * 0.40;
-    document.getElementById('discount').textContent = `Your 40% discount is: €${discount.toFixed(2)}`;
+    document.getElementById('discount').textContent = `Il tuo sconto del 40% è di: €${discount.toFixed(2)}`;
   } else {
-    document.getElementById('discount').textContent = "You are not eligible for any discount";
+    document.getElementById('discount').textContent = "Non hai diritto a nessuno sconto";
   }
 
   total -= discount;
 
-  document.getElementById('totalFinal').textContent = `The total cost of the ticket is: €${total.toFixed(2)}`;
+  document.getElementById('totalFinal').textContent = `Il prezzo totale è di: €${total.toFixed(2)}`;
 });
