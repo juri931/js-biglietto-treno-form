5e6f488948afaa1ad13b0c79c90f12d1941eb6bf
juri931 <impa.mat@hotmail.it>
2024-02-21 18:44:25 +0100

    [fix] some fixes

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ document.getElementById('myForm').addEventListener('submit', function (event) {
 
   const userName = document.getElementById('inputName');
   const distanceKm = document.getElementById('inputKm');
-  const ageSelect = document.getElementById('formSelect');
+  const ageSelect = document.getElementById('ageSelect');
 
   console.log(`Nome: ${userName.value}`);
   console.log(`Distanza: ${distanceKm.value} KM`);
@@ -14,25 +14,23 @@ document.getElementById('myForm').addEventListener('submit', function (event) {
   ageSelect.textContent = ageSelect.value;
 
   const basePricePerKm = 0.21;
-  const totalDistance = parseFloat(distanceKm.value);
+  const totalDistance = distanceKm.value;
   let total = totalDistance * basePricePerKm;
 
   document.getElementById('total').textContent = `Costo della tratta: €${total.toFixed(2)}`;
 
-  let discount = 0;
+  let discount20 = total * 0.20;
+  let discount40 = total * 0.40;
 
-
-  if (ageSelect.value = minor) {
-    discount = total * 0.20;
-    document.getElementById('discount').textContent = `Il tuo sconto del 20% è di: €${discount.toFixed(2)}`;
-  } else if (ageSelect.value = major) {
-    discount = total * 0.40;
-    document.getElementById('discount').textContent = `Il tuo sconto del 40% è di: €${discount.toFixed(2)}`;
+  if (ageSelect.value = "1") {
+    total = (total - discount20);
+    document.getElementById('discount').textContent = "Il tuo sconto del 20% è di: €" + ((discount20).toFixed(2));
+  } else if (ageSelect.value = "3") {
+    total = (total - discount40);
+    document.getElementById('discount40').textContent = "Il tuo sconto del 40% è di: €" + ((discount40).toFixed(2));
   } else {
     document.getElementById('discount').textContent = "Non hai diritto a nessuno sconto";
   }
 
-  total -= discount;
-
-  document.getElementById('totalFinal').textContent = `Il prezzo totale è di: €${total.toFixed(2)}`;
+  document.getElementById('totalFinal').textContent = "Il prezzo totale è di: €" + ((total).toFixed(2));
 });
